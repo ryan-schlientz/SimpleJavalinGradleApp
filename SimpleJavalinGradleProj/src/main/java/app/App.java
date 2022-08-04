2e9d8d5c3c546b8f257a5cbed2b1f3f074292a34
ryan-schlientz <omega101_ryan@yahoo.com>
2022-08-04 17:08:58 -0400

    updates

--- SimpleJavalinGradleProj/src/main/java/app/App.java
+++ SimpleJavalinGradleProj/src/main/java/app/App.java
@@ -5,13 +5,15 @@ import io.javalin.core.JavalinConfig;
 
 public class App {
 
+    static int counter = 0;
+
     public static void main(String[] args) {
 
         Javalin app = Javalin.create(JavalinConfig::enableCorsForAllOrigins);
 
         app.get("/", ctx -> { ctx.result("Simple Javalin App using Gradle"); });
         app.get("/hello", ctx -> { ctx.result("Hello World!"); });
-        app.get("/test", ctx -> {  ctx.status(201); ctx.result("Test Test Test"); });
+        app.get("/test", ctx -> {  ctx.status(201); ctx.result("Test " + ++counter); });
 
         app.start();
 
